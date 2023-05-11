e4b19855099837659c53e95238cc2d8c77f09820
med_Sk09 <109962323+medXPS@users.noreply.github.com>
2023-05-11 15:34:56 +0100

    Add files via upload

---
+++ src/test/java/ma/e_Banking/ebankingApp/EBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.e_Banking.ebankingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
