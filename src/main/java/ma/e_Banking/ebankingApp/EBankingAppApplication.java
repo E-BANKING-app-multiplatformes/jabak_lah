0d5d826a0b69fae0872b4c5d61549b3a67c8bc78
cimoDevOps <m.ammaha2144@uca.ac.ma>
2023-05-15 18:48:18 +0100

    pushing..v3

---
+++ src/main/java/ma/e_Banking/ebankingApp/EBankingAppApplication.java
@@ -0,0 +1,13 @@
+package ma.e_Banking.ebankingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBankingAppApplication.class, args);
+	}
+
+}
